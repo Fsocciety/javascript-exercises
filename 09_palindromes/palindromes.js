74f9a61c90ea162107d227657e24b54e8808a30f
Uros <dixypixy007@gmail.com>
2023-04-09 16:19:29 +0200

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function(a) {
+    let string = a.toLowerCase().replace(/[^a-z]/g, "");
+    return string.split("").reverse().join("") == string;
 };
 
 // Do not edit below this line
